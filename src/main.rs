46eaf5d6dd673c27c8d0a4eaf4388d30069c25d7
Jason Turner <jason@emptycrate.com>
2020-06-02 13:53:43 -0600

    Why can I not parameterize the function pointer?

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,112 @@ use std::time::Duration;
 
 mod test;
 
-
-/*
-fn generic_function<'T, Type1, Type2>(val:&'T Type1, val2:&'T Type2) -> &'T Type1
-{
-    return val;
-
-    // @jntrnr on Twitter
+// is it possible to pass width and height as compile-time constants?
+#[derive(Clone)]
+struct Life<ValueType> {
+    width: u32,
+    height: u32,
+    wrap_indexes: bool,
+    data: Vec<ValueType>,
+    default_value: ValueType,
+    next_value: fn(u32,u32,&Life) -> ValueType,
 }
-*/
+
+impl<ValueType: Copy + PartialEq> Life<ValueType> {
+    fn wrap_coord(value: i64, max_value: u32) -> u32 {
+        if value < 0 {
+            return max_value + ((value % (max_value as i64)) as u32);
+        } else {    
+            return (value % (max_value as i64)) as u32;
+        }
+    }
+
+    fn count_neighbors(&self, x: u32, y: u32, expected: ValueType) {
+        let count = 0;
+
+        for rel_x in -1..=1 {
+            for rel_y in -1..=1 {
+                if !(rel_x == 0 && rel_y == 0)
+                    && *self.at(x as i64 + rel_x, y as i64 + rel_y) == expected
+                {
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    fn next(&self) -> Life<ValueType> {
+        let mut result: Life<ValueType> = self.clone();
+
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let next = (self.next_value)(x,y,self);
+                result.at_mut(x as i64, y as i64) = next;
+            }
+        }
+
+        return result;
+    }
+
+    fn at(&self, x: i64, y: i64) -> &ValueType {
+        if !self.wrap_indexes
+            && (x < 0 || y < 0 || x >= (self.width as i64) || y >= (self.height as i64))
+        {
+            return &self.default_value;
+        }
+
+        // x and y should both, mathematically, be >= 0 and < width or height when this is called
+        return self
+            .data
+            .get(
+                (Life::<ValueType>::wrap_coord(y, self.height) * self.height
+                    + Life::<ValueType>::wrap_coord(x, self.width)) as usize,
+            )
+            .unwrap();
+    }
+
+    // allow signed integral large enough to index any u32 * u32 position
+    // but allow wrap around for ease of use for edge cases
+    fn at_mut(&mut self, x: i64, y: i64) -> &mut ValueType {
+        if x < 0 || y < 0 || x >= (self.width as i64) || y >= (self.height as i64) {
+            assert!(self.wrap_indexes);
+        }
+
+        // x and y should both, mathematically, be >= 0 and < width or height when this is called
+        return self
+            .data
+            .get_mut(
+                (Life::<ValueType>::wrap_coord(y, self.height) * self.height
+                    + Life::<ValueType>::wrap_coord(x, self.width)) as usize,
+            )
+            .unwrap();
+    }
+}
+
+fn next_value(x:u32, y:u32, life: &Life<bool>) -> bool
+{
+    return false;
+}
+
+// google says rust doesn't have constructors?
+fn make_life<ValueType: Copy + PartialEq>(
+    width: u32,
+    height: u32,
+    default_value: ValueType,
+) -> Life<ValueType> {
+    let life = Life::<ValueType> {
+        width: width,
+        height: height,
+        wrap_indexes: true,
+        data: vec![default_value; (width * height) as usize],
+        default_value: default_value,
+        next_value: next_value
+    };
+    return life;
+}
 
 fn main() -> Result<(), String> {
-    //test::funny_name();
-    //generic_function(&4);
-    //test::other_function();
-
+    let life = make_life(32, 32, false);
 
     let context = sdl2::init();
 
